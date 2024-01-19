cc12d5fe96a071fba571e7a69d9530ad0b740131
Roman Sachuck <roman.sachuk.1997@mail.ru>
2024-01-19 23:02:43 +0300

    .

---
+++ 13.6.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace _13._6._1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> list = new List<char>();
+            LinkedList<char> linkedList = new LinkedList<char>();
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            using (StreamReader sr = File.OpenText(@"C:\Users\Роман\Desktop\text.txt"))
+            {
+                while (sr.Peek() != -1)
+                    list.Add((char)sr.Read());
+            }
+            stopWatch.Stop();
+            Console.WriteLine("RunTime list.Add " + stopWatch.Elapsed);
+
+            stopWatch.Restart();
+            using (StreamReader sr = File.OpenText(@"C:\Users\Роман\Desktop\text.txt"))
+            {
+                while (sr.Peek() != -1)
+                    linkedList.AddLast((char)sr.Read());
+            }
+            stopWatch.Stop();
+            Console.WriteLine("RunTime Linkedlist.AddLast " + stopWatch.Elapsed);
+
+            list.Clear();
+            stopWatch.Restart();
+            using (StreamReader sr = File.OpenText(@"C:\Users\Роман\Desktop\text.txt"))
+            {
+                while (sr.Peek() != -1)
+                    list.Insert(list.Count / 2, (char)sr.Read());
+            }
+            stopWatch.Stop();
+            Console.WriteLine("RunTime List.Insert " + stopWatch.Elapsed);
+        }
+    }
+}
---
+++ 13.6.2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _13._6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ResultRange = 10;
+            var text = File.ReadAllText(@"C:\Users\Роман\Desktop\text.txt");
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            string[] words = noPunctuationText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> wordCounter = new Dictionary<string, int>();
+            string[] resultWords = new string[ResultRange];
+            int[] resultNembers = new int[ResultRange];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!wordCounter.ContainsKey(words[i]))
+                    wordCounter.Add(words[i], 0);
+                else wordCounter[words[i]]++;
+            }
+
+            for (int i = 0; i < ResultRange; i++)
+            {
+                int currentValue = 0;
+
+                foreach (var word in wordCounter)
+                {
+                    if (word.Value > currentValue)
+                    {
+                        resultWords[i] = word.Key;
+                        resultNembers[i] = word.Value;
+                        currentValue = word.Value;
+                    }
+                }
+
+                wordCounter.Remove(resultWords[i]);
+            }
+
+            for (int i = 0; i < ResultRange; i++)
+                Console.WriteLine($"{i + 1}. {resultWords[i]} = {resultNembers[i]}");
+        }
+    }
+}
+
